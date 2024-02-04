4668d8df72d1079bba54ac60be5c486e1cea816b
Nikusha Jvarsheishvili <nikushajvarsheishvili28@gmail.com>
2024-02-04 14:11:43 +0400

    error fix

--- src/Formik-components/AddReviewForm.js
+++ src/Formik-components/AddReviewForm.js
@@ -1,9 +1,10 @@
-import { string, object, number } from 'Yup'
+import { string, object, number } from "Yup";
 
 export const validationSchema = object({
-    headline: string().min(4).trim().required('Please enter your headline'),
-    writtenReview: string().min(15).trim().required('Please enter your written review'),
-    starRating: number().required('Please select a star rating')
-})
-
-
+  headline: string().min(4).trim().required("Please enter your headline"),
+  writtenReview: string()
+    .min(15)
+    .trim()
+    .required("Please enter your written review"),
+  starRating: number().required("Please select a star rating"),
+});
